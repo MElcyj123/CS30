3076a24ad041b048da62bc72cbd6603d76ada78d
MElcyj123 <160177908+MElcyj123@users.noreply.github.com>
2024-02-26 13:05:11 -0600

    State Variable Challenge
    MELODY C and NANCY Y

--- sketch.js
+++ sketch.js
@@ -1,15 +1,49 @@
-// Project Title
-// Your Name
-// Date
+// State Variable Challenge
+// Melody C and Nancy Y
+// 2/26/2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+
+let x = 0; 
+let sideLength = 50; 
+let state = 1; 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  // set the start point
+  y = height - sideLength;
 }
 
 function draw() {
   background(220);
+
+// draw the rectangle
+  fill(255, 255, 255);
+  square(x, y, sideLength);
+
+  // animation main code
+  if (state === 1) {
+    x += 5;
+    if (x + sideLength > width) {
+      state = 2;
+    }
+  } else if (state === 2) {
+    y -= 5;
+    if (y < 0) {
+      y = 0;
+      state = 3;
+    }
+  } else if (state === 3) {
+    x -= 5;
+    if (x < 0) {
+      x = 0;
+      state = 4;
+    }
+  } else if (state === 4) {
+    y += 5;
+    if (y + sideLength > height) {
+      state = 1;
+    }
+  }
 }
