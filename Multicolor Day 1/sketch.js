a9877989eff86e19fba6ee23120f886b8f8f84d9
MElcyj123 <160177908+MElcyj123@users.noreply.github.com>
2024-03-05 15:17:59 -0600

    Multicolor Day 2

--- Multicolor Day 1/sketch.js
+++ Multicolor Day 1/sketch.js
@@ -6,10 +6,13 @@ let squareSize = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noLoop();
+  document.addEventListener("contextmenu", event => event.preventDefault())
 }
 
 function draw() {
   squareLoops();
+
 }
 
 function squareLoops(){
@@ -17,6 +20,34 @@ function squareLoops(){
     for(let y=0; y < height; y += squareSize){
       stroke(0);
       square(x,y,squareSize);
+      fill(random(250), random(250), random(250));
     }
+  }
+}
+
+function mousePressed(){
+  if(20 - squareSize >= 5){
+    if(mouseButton === LEFT){
+      squareSize = squareSize + 5;
+    }
+    if(mouseButton === RIGHT){
+      squareSize = squareSize - 0.005;
+    }
+  }
+  else{
+    if(mouseButton === LEFT){
+      squareSize = squareSize + 5;
+    }
+    if(mouseButton === RIGHT){
+      squareSize = squareSize - 5;
+    }
+  }
+  
+  squareLoops();
+}
+
+function keyPressed(){
+  if(key === "a"){
+    squareLoops();
   }
 }
