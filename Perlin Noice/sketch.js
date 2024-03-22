e5ae1758c14e086b657372e6fdbd03a1d40c39d3
MElcyj123 <160177908+MElcyj123@users.noreply.github.com>
2024-03-22 15:22:47 -0600

    day...(idk how many days... after several days working home and school, they re messy)

--- Perlin Noice/sketch.js
+++ Perlin Noice/sketch.js
@@ -2,75 +2,80 @@
 // Melody CHen
 // 3/11/2024
 
-let rectWidth, rectHeight = 1;
-let totalHeight, averageHeight;
-let rectCount;
-let topX, topY;
+//global variables
+let rectWidth = 1;
+let rectHeight = 1; 
+let totalHeight = 0;
+let averageHeight = 0; 
+let rectCount = 0;
+let topX = 0; //highest coordinate
+let topY = 0;
+let heightShift = 1;
+let heightTime = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
-  rectMode(CORNERS); //CHANGE THIS!!!
+  rectMode(CORNERS);
   generateTerrain();
 }
 
 function draw() {
-  drawFlag();
+  background(255);
+  heightTime = 0 + heightShift; // panning
+  heightShift += 0.1;
+  generateTerrain();
 }
 
-function generateTerrain(){
-  // using a single liip, generate a bunch of side-to-side
-  // rectangles of varying height(pattern, random, noise)
-  let topX = 0;
-  let topY = 0;
-  let totalHeight = 0;
-  let heightTime = 0;
-  let noiseShift = 0.01;
-  let rectCount = 0;
+
+// perlin noise
+function generateTerrain() {
   fill(0);
-  for(let x = 0; x < width; x += rectWidth){
+  for (let x = 0; x < width; x += rectWidth) {
     rectHeight = noise(heightTime);
-    rectHeight = map(rectHeight,0,1,0,height);
+    rectHeight = map(rectHeight, 0, 1, 0, height);
     rect(x, height, x + rectWidth, height - rectHeight);
-    heightTime += noiseShift;
+    heightTime += 0.01;
     totalHeight += rectHeight;
-    rectCount += 1;
-    if(topY < rectHeight){
-      topY = rectHeight; 
+    rectCount ++ ;
+    if (topY < rectHeight) {
+      topY = rectHeight;
       topX = x;
     }
-    // averageHeight = totalHeight / rectCount;
-    // drawAverage();
-    drawFlag(topX, height - topY);
   }
+  drawFlag(topX, height - topY); // whole scene height - yheight
+  averageHeight = totalHeight / rectCount; 
+  drawAverage();
 
+ 
 }
-
-// function keyPressed(){
-//   if(keyCode === LEFT_ARROW){
-//     if(rectWidth > 1){
-//       rectWidth = rectWidth -1;
-//       clear();
-//     }
-//     else (rectWidth = rectWidth - 0.001);
-//   }
-//   generateTerrain();
-//   if(keyCode === RIGHT_ARROW){
-//       rectWidth = rectWidth +1;
-//       clear();
-//   }
-//   generateTerrain();
-// }
-
-// function drawAverage() {
-//   fill(0, 0, 255);
-//   rect(0, height - averageHeight, width, height - averageHeight + 5);
-// }
 
 // Draw the flag
 function drawFlag(x, y) {
   stroke(0);
   fill(0);
   line(x, y, x, y - 30);
-  triangle(x, y - 30, x - 10 / 2, y - 30, x, y - 30 - 10);
+  triangle(x, y - 30, x + 10 / 2, y - 30, x, y - 40);
+}
+
+// The average line
+function drawAverage() {
+  fill(255,0,0);
+  rect(0, height - averageHeight, width, height - averageHeight + 5);
+}
+
+// key pressed increase or decrease
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    if(rectWidth > 1){
+      rectWidth -= 1;
+      clear();
+      generateTerrain();
+    }
+  }
+  else if(keyCode === RIGHT_ARROW){
+    rectWidth += 1;
+    clear()
+    generateTerrain();
+  }
 }
