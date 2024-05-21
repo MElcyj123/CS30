0302bf3d3e0365fa6d72788cc3efe71a4aa3fb4e
MElcyj123 <160177908+MElcyj123@users.noreply.github.com>
2024-05-21 14:31:04 -0600

    Day 4
    I tried to upload at home but i forgot how to set up github. so this is day 4 including weekends

--- Puzzle Game/sketch.js
+++ Puzzle Game/sketch.js
@@ -2,6 +2,7 @@
 // Melody Chen
 // 5/14/20
 
+
 let NUM_ROWS = 4;
 let NUM_COLS = 5;
 let rectWidth, rectHeight;
@@ -11,7 +12,10 @@ let gridData = [[0,0,0,0,0],
                 [0,255,0,0,0],
                 [255,255,255,0,0]];
 
+
+let pattern = 'cross'; // set the initial pattern to cross
 let overlay;
+let col, row;
 
 
 function setup() {
@@ -20,16 +24,21 @@ function setup() {
   rectWidth = width/NUM_COLS;
   rectHeight = height/NUM_ROWS;
 
+
   randomizedStartingArrangement();
 }
+
 
 function draw() {
   background(220);
   determineActiveSquare();   //figure out which tile the mouse cursor is over
   drawGrid();                //render the current game board to the screen (and the overlay)
   youWin();
-  mouseTrack();
+  drawOverlay();
 }
+
+
+
 
 
 
@@ -41,13 +50,22 @@ function mousePressed(){
     flip(col, row);
   } else {
     // cross-shaped pattern flips on a mouse click
-    flip(currentCol, currentRow);
-    flip(currentCol - 1, currentRow);
-    flip(currentCol + 1, currentRow);
-    flip(currentCol, currentRow - 1);
-    flip(currentCol, currentRow + 1);
+    if(pattern === 'cross'){
+      flip(currentCol, currentRow); // same direction with drawCrossOverlay
+      flip(currentCol - 1, currentRow);
+      flip(currentCol + 1, currentRow);
+      flip(currentCol, currentRow - 1);
+      flip(currentCol, currentRow + 1);
+    }
+    else if(pattern === 'square'){ // same direction with drawSquareOverlay
+      flip(currentCol, currentRow);
+      flip(currentCol - 1, currentRow);
+      flip(currentCol - 1, currentRow - 1);
+      flip(currentCol, currentRow - 1);
+    }
   }
 }
+
 
 function flip(col, row){
   // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
@@ -60,22 +78,26 @@ function flip(col, row){
   }
 }
 
+
 function determineActiveSquare(){
   // An expression to run each frame to determine where the mouse currently is.
   currentRow = int(mouseY / rectHeight);
   currentCol = int(mouseX / rectWidth);
 }
 
+
 function drawGrid(){
   // Render a grid of squares - fill color set according to data stored in the 2D array
   for (let x = 0; x < NUM_COLS ; x++){
     for (let y = 0; y < NUM_ROWS; y++){
-      fill(gridData[y][x]); 
+      fill(gridData[y][x]);
       rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
     }
   }
 }
 
+
+//Win Condition
 function youWin(){
   let win = true;
   for (let x = 0; x < NUM_COLS; x++) {
@@ -87,6 +109,7 @@ function youWin(){
     }
   }
 
+
   if (win) {
     textSize(32);
     fill(103, 52, 242);
@@ -95,6 +118,8 @@ function youWin(){
   }
 }
 
+
+//Randomized Starting Arrangement
 function randomizedStartingArrangement() {
   for (let x = 0; x < NUM_COLS; x++) {
     for (let y = 0; y < NUM_ROWS; y++) {
@@ -103,10 +128,59 @@ function randomizedStartingArrangement() {
   }
 }
 
-function mouseTrack(){
-  overlay.fill(255,0,0,100);
-  // if (keyIsPressed && keyCode === SHIFT) { // Cheater Cheater
-  //   overlay.rect(col * 50, row * 50, 50, 50);
-  // }
 
+//overlay
+function drawOverlay() {
+  fill(0, 255, 0, 100); // green
+  if (keyIsPressed && keyCode === SHIFT) { // Cheater Cheater
+    drawOverlayRect(currentCol, currentRow);
+  }
+  //flipping in cross and square
+  else if (pattern === 'cross') {
+    drawCrossOverlay(currentCol, currentRow);
+  }
+  else if (pattern === 'square') {
+    drawSquareOverlay(currentCol, currentRow);
+  }
 }
+
+
+function drawCrossOverlay(col,row){
+  drawOverlayRect(col, row); // center(where the mouse is pointed)
+  drawOverlayRect(col - 1, row); // left
+  drawOverlayRect(col + 1, row); // right
+  drawOverlayRect(col, row - 1); // down
+  drawOverlayRect(col, row + 1); // up
+}
+
+
+function drawSquareOverlay(col,row){
+  drawOverlayRect(col, row);
+  drawOverlayRect(col - 1, row);
+  drawOverlayRect(col - 1, row - 1);
+  drawOverlayRect(col, row - 1);
+}
+
+
+function drawOverlayRect(col, row) {
+  for (let x = 0; x < NUM_COLS; x++) {
+    for (let y = 0; y < NUM_ROWS; y++) {
+      if (x === col && y === row) {
+        rect(x * rectWidth, y * rectHeight, rectWidth, rectHeight);
+      }
+    }
+  }
+}
+
+
+function keyPressed(){
+  if(key === " "){
+    if(pattern === 'cross'){
+      pattern = 'square';
+    }
+    else if(pattern === 'square'){
+      pattern = 'cross'
+    }
+  }
+}
+
