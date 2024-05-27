f9b5422b0a622454214e37384d4ba2c98b5076d8
MElcyj123 <160177908+MElcyj123@users.noreply.github.com>
2024-05-27 15:24:14 -0600

    Final Project Day 1
    finish the HTML background codes

---
+++ Final Project/sketch.js
@@ -0,0 +1,19 @@
+// Final Project
+// Melody Chen and Nancy Yang
+// May 24th, 2024
+
+let captainCanuck;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  captainCanuck = new Sprite;
+  
+}
+
+
+function draw() {
+  background(220);
+  
+}
+
